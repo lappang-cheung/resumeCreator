462705def9f9a18e52f9ea42932dbab21d1794e3
Leo Cheung <lappangcheung@gmail.com>
2018-11-05 09:55:47 -0500

    create the env config for mongodb

---
+++ server/config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb://localhost:27017/createResume",
+    secretOrKey: "This is a testing environment"
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
+// Required modules
 const express = require('express')
+const mongoose = require('mongoose')
+
+// Custom variables
 const app = express()
 const PORT = process.env.PORT || 5000
 
+// Routes
 app.get('/', async (req, res) => res.send("Hello"))
 
+// App initialize
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
