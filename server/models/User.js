0b66fb36a464014b6519606dd063fbfa4cd0e432
Leo Cheung <lappangcheung@gmail.com>
2018-11-05 10:17:51 -0500

    create user model

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = User = mongoose.model('users', UserSchema)
