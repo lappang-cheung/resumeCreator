c1a19aa8d776ab3a5af9009a71a7d9661e7293e4
Leo Cheung <lappangcheung@gmail.com>
2018-11-05 09:59:32 -0500

    update the server to connect to mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 // Required modules
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 // Custom variables
 const app = express()
 const PORT = process.env.PORT || 5000
+const keys = require('./config/keys')
+
+// BodyParser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Routes
 app.get('/', async (req, res) => res.send("Hello"))
 
-// App initialize
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+// Connecting to DB and setting port
+app.listen(PORT, async() => {
+    await mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
+    console.log(`Listening on port ${PORT}`)
+})
