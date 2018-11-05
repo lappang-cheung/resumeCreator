ade9a09f12b9abc3088170200f2bddeff4879f5c
Leo Cheung <lappangcheung@gmail.com>
2018-11-05 11:00:24 -0500

    add the payload and token to login

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,8 +1,9 @@
 // Required modules
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
+const jwt = require('jsonwebtoken')
 // Custom modules
 const helpers = require('../../util/helpers')
+const keys = require('../../config/keys')
 // Load the model
 const User = require('../../models/User')
 
@@ -43,16 +44,39 @@ router.post('/register', async(req, res) => {
 // @desc    Login user route
 // @access  Public
 router.post('/login', async(req, res) => {
+    // Destructing for email and password from the body
     const { email, password } = req.body
-    
+    // Store the state of the user
     const user = await User.findOne({ email })
-
+    // Check if user email exist
     if(!user){
+        // Return 404 if the email does not exist
         return res.status(404).json({ email: 'Email not found'})
     }else{
+        // Create the hash password and compare the hash password
         if(helpers.hash(password) === user.password){
-            res.json({ message: 'Success'})
+            // Create the payload
+            const payload = {
+                id: user.id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    avatar: user.avatar
+            }
+            // JSON web token
+            jwt.sign(
+                payload,
+                keys.secretOrKey,
+                { expiresIn: 3600 },
+                (err, token) => {
+                    res.json({
+                        success: true,
+                        token: 'Bearer ' + token
+                    })
+                }
+            )
         }else{
+            // Return 404 when password is incorrect
             return res.status(400).json({ password: 'Password is incorrect'})
         }
     }
