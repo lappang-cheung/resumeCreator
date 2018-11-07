3e8576776ba605355a02e0f2962d6fd39f1c1eb3
Leo Cheung <lappangcheung@gmail.com>
2018-11-07 16:09:13 -0500

    add the routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom' 
 
 import Navbar from '../components/layout/Navbar'
 import Landing from '../components/layout/Landing'
@@ -7,11 +8,13 @@ import Footer from '../components/layout/Footer'
 class App extends Component {
     render() {
         return (
-            <>
-                <Navbar />
-                <Landing />
-                <Footer />
-            </>
+            <Router>
+                <>
+                    <Navbar />
+                    <Route exact path="/" component={Landing}/>
+                    <Footer />
+                </>
+            </Router>
         )
     }
 }
